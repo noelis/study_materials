61695684ec9f30ad1a40390674a7ff8e2eed95d4
Noelis Soto <soto.noelis@gmail.com>
2016-12-19 12:17:37 -0800

    Added logic required to solve problem. Even with logic in place, the best string percentage I can get is .32 :(

--- problem_solving.py
+++ problem_solving.py
@@ -3,7 +3,9 @@
 
 import random
 
-""" Write a function that generates a string that is 27 characters long by choosing random
+""" Scope  of problem:
+
+    Write a function that generates a string that is 27 characters long by choosing random
     letters from the 26 letters in the alphabet plus the space.
 
     We’ll write another function that will score each generated string by comparing the
@@ -15,30 +17,49 @@ import random
     To make it easier to follow your program’s progress this third function should 
     print out the best string generated so far and its score every 1000 tries."""
 
+
 def generate_string():
     """Generates a string that is 27 characters long by choosing random letters 
     from the 26 letters in the alphabet plus a space."""
 
     characters = "abcdefghijklmnopqrstuvwxyz "
 
-    # new_str = ""
-
-    # for i in range(28):
-    #     new_str += random.choice(characters)
-
-    # return new_str
-
     return "".join([random.choice(characters) for i in range(28)])
 
+
 def score_string(test_str):
+    """ Scores each generated string by comparing the randomly generated string to the goal. """
+
     string_goal = "methinks it is like a weasel"
 
-    if test_str == string_goal:
-        return True
-    else:
-        return "boo!"
+    count_correct = 0
 
-test = generate_string()
-print test
-print score_string(test)
+    for i in range(28):
+        if test_str[i] == string_goal[i]:
+            count_correct += 1
 
+    return count_correct/28.0
+
+
+def repeat_again():
+    """A third function will repeatedly call generate and score, then if all of the letters 
+    are correct we are done. If the letters are not correct then we will generate a whole new string."""
+ 
+    best = 0
+    test_str = generate_string()
+    score = score_string(test_str)
+    count = 0
+
+    while score < 1:
+        count += 1
+        if count % 1000 == 0:
+            print test_str, best
+        elif score > best:
+            print score, test_str
+            best = score
+        test_str = generate_string()
+        score = score_string(test_str)
+
+
+
+print repeat_again()
